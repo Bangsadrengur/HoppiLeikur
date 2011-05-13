0bd680bed810d950c21df12c52b5727fb3775d9e
Heimir Þór Kjartansson <hemmi.hk@gmail.com>
2011-05-13 14:07:38 +0000

    Up to part 5

--- game.js
+++ game.js
@@ -3,10 +3,12 @@ var width = 320,
     gLoop,
     c = document.getElementById('c'),
     ctx = c.getContext('2d');
+
 c.width = width;
 c.height = height;
 
-var clear = function() {
+
+var clear = function(){
     ctx.fillStyle = '#d0e7f9';
     ctx.beginPath();
     ctx.rect(0, 0, width, height);
@@ -63,6 +65,53 @@ var player = new(function() {
     that.isFalling = false;
     that.jumpSpeed = 0;
     that.fallSpeed = 0;
+
+    that.jump = function() {
+        if(!that.isJumping && !that.isFalling) {
+            that.fallSpeed = 0;
+            that.isJumping = true;
+            that.jumpSpeed = 17;
+        }
+    }
+
+    that.checkJump = function() {
+        that.setPosition(that.X, that.Y - that.jumpSpeed);
+        that.jumpSpeed--;
+        if(that.jumpSpeed == 0) {
+            that.isJumping = false;
+            that.isFalling = true;
+            that.fallSpeed = 1;
+        }
+    }
+
+    that.checkFall = function() {
+        if(that.Y < height - that.height) {
+            that.setPosition(that.X, that.Y + that.fallSpeed);
+            that.fallSpeed++;
+        }
+        else {
+            that.fallStop();
+        }
+    }
+
+    that.fallStop = function() {
+        that.isFalling = false;
+        that.fallSpeed = 0;
+        that.jump();
+    }
+
+    that. moveLeft = function() {
+        if(that.X > 0) {
+            that.setPosition(that.X - 5, that.Y);
+        }
+    }
+
+    that.moveRight = function() {
+        if(that.X + that.width < width) {
+            that.setPosition(that.X + 5, that.Y);
+        }
+    }
+
     that.draw = function() {
         try {
             ctx.drawImage(that.image, 0, that.height * that.actualFrame, that.width, that.height, that.X, that.Y, that.width, that.height);
@@ -81,12 +130,91 @@ var player = new(function() {
     }
 })();    
 
+var nrOfPlatforms = 7,
+    platforms = [],
+    platformWidth = 70,
+    platformHeight = 20;
+
+var Platform = function(x, y, type) {
+    var that = this;
+    that.firstColor = '#FF8C00';
+    that.secondColor = '#EEEE00';
+    that.onCollide = function() {
+        player.fallStop();
+    };
+    if(type === 1) {
+        that.firstColor = '#AADD00';
+        that.secondColor = '#698B22';
+        that.onCollide = function() {
+            player.fallStop();
+            player.jumpSpeed = 50;
+        };
+    }
+    that.x = ~~x;
+    that.y = y;
+    that.type = type;
+    that.draw = function() {
+        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+        var gradient = ctx.createRadialGradient(that.x + (platformWidth/2), that.y + (platformHeight/2), 5, 
+                that.x + (platformWidth/2), that.y + (platformHeight/2), 45);
+    var gradient = ctx.createRadialGradient(that.x + (platformWidth/2), that.y + (platformHeight/2), 5, 
+            that.x + (platformWidth/2), that.y + (platformHeight/2), 45);
+    gradient.addColorStop(0, that.firstColor);
+    gradient.addColorStop(1, that.secondColor);
+    ctx.fillStyle = gradient;
+    ctx.fillRect(that.x, that.y, platformWidth, platformHeight);
+    };
+    return that;
+};
+
+var generatePlatforms = function() {
+    var position = 0, type;
+    for(var i = 0; i < nrOfPlatforms; i++) {
+        type = ~~(Math.random()*5);
+        if(type == 0) type = 1;
+        else type = 0;
+        platforms[i] = new Platform(Math.random()*(width - platformWidth), position, type);
+        if(position < height - platformHeight)
+            position += ~~(height / nrOfPlatforms);
+    }
+}();
+
+document.onmousemove = function(e) {
+    if(player.X + c.offsetLeft > e.pageX) {
+        player.moveLeft();
+    }
+    else if(player.X + c.offsetLeft < e.pageX) {
+        player.moveRight();
+    }
+}
+
 player.setPosition(~~((width - player.width)/2), ~~((height - player.height)/2));
+player.jump();
+
+var checkCollision = function() {
+    platforms.forEach(function(e, ind) {
+        if(
+            (player.isFalling) &&
+             (player.X < e.x + platformWidth) &&
+             (player.X + player.width > e.x) &&
+             (player.Y + player.height > e.y) &&
+             (player.Y + player.height < e.y + platformHeight)
+        ) {
+            e.onCollide();
+        }
+    })
+}
 
 var GameLoop = function() {
     clear();
-    MoveCircles(5);
+    //MoveCircles(5);
     DrawCircles();
+    if(player.isJumping) player.checkJump();
+    if(player.isFalling) player.checkFall();
+    platforms.forEach(function(platform) {
+        platform.draw();
+    });
+    checkCollision();
     player.draw();
     gLoop = setTimeout(GameLoop, 1000 / 50);
 }
